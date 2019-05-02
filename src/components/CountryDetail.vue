e8dca859afe05c7f51a9152c5aaae86ec7b489c3
dataSlayer64275 <ess.knst@gmail.com>
2019-05-02 09:27:43 +0100

    Initial commit

---
+++ src/components/CountryDetail.vue
@@ -0,0 +1,17 @@
+<template lang="html">
+  <div>
+    <h3>{{selectedCountry.name}}</h3>
+    <h3>{{selectedCountry.capital}}</h3>
+    <h3>{{selectedCountry.population}}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'country-detail',
+  props: ['selected-country']
+}
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ src/components/ListItem.vue
@@ -0,0 +1,21 @@
+<template lang="html">
+  <li v-on:click="listClick">{{country.name}}</li>
+</template>
+
+<script>
+
+import {eventBus} from '../main.js'
+
+export default {
+  name: 'list-item',
+  props: ['country'],
+  methods: {
+    listClick(){
+      eventBus.$emit('country-selected', this.country)
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
